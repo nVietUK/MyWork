d5ac83ac830673955796a1242fc97dd54aef9c00
nVietUK <vietuk.nguyen.git@icloud.com>
2022-03-03 16:17:32 +0700

    main

--- shopping.cpp
+++ shopping.cpp
@@ -1,78 +1,58 @@
 #include <bits/stdc++.h>
 
-typedef unsigned long long ull;
-
-template <typename T>
-    struct wrapped_array {
-        wrapped_array(T* first, T* last)
-            : begin_ {first}, end_ {last} {}
-        wrapped_array(T* first, std::ptrdiff_t size)
-            : wrapped_array {first, first + size} {}
-
-        T* begin() const noexcept { return begin_; }
-        T* end() const noexcept { return end_; }
-
-        T* begin_;
-        T* end_;
-    };
-
 using namespace std;
 
-unsigned n, *arr1, *arr2, in, l, r;
-ull d;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<unsigned, unsigned> uu;
 
-void build(unsigned *in) {
-    for (unsigned i = n-1; i >= n>>1; i--) 
-        in[i] = in[i<<1] * in[i<<1|1];
-}
+template <typename T> struct wrapped_array {
+    wrapped_array(T* first, T* last)
+        : begin_ {first}, end_ {last} {}
+    wrapped_array(T* first, std::ptrdiff_t size)
+        : wrapped_array {first, first + size} {}
 
-unsigned run(unsigned be, unsigned en, const unsigned *in, bool **vi, unsigned **dp) {
-    if (vi[be][en]) 
-        return dp[be][en];
-    vi[be][en] = true;
+    T* begin() const noexcept { return begin_; }
+    T* end() const noexcept { return end_; }
 
-    if (be == en)
-        return dp[be][en] = in[be];
+    T* begin_;
+    T* end_;
+};
 
-    for (unsigned i = be+1; i < en; i++)
-        return 1;
-}
-
-unsigned get(const unsigned *in, unsigned be, unsigned en) {
-    bool **vi = new bool*[en]; for (auto&& e : wrapped_array(vi, en)) e = new bool[en];
-    unsigned **dp = new unsigned*[en]; for (auto&& e : wrapped_array(dp, en)) e = new unsigned[en];
-
-    return run(be, en, in, vi, dp);
-}
+unsigned n;
+ll *arr, *p1, *p2, d;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-#ifndef ONLINE_JUDGE
+#if (__linux__ || _WIN32)
     freopen("shopping.inp", "r", stdin);
     freopen("shopping.out", "w", stdout);
 #endif
 
-    cin >> n >> d;
-    arr1 = new unsigned[4*(n/2)]; arr2 = new unsigned[4*(n/2)];
-    for (auto&& ele : wrapped_array(arr1, 2*n)) ele = 1;
-    for (auto&& ele : wrapped_array(arr2, 2*n)) ele = 1;
-    if (n%2) {
-        l = r = n-1;
-        for (unsigned i = 0; i < n; i++) {
-            cin >> in;
-            if (i > 1) arr1[n+i-1] = in;
-            if (i < n-1) arr2[n+i] = in;
+    cin >> n >> d; 
+    ll res = 0;
+    arr = new ll[n+1]; p1 = new ll[n+1]; p2 = new ll[n+1];
+    for (unsigned i = 1; i <= n; i++) {
+        cin >> arr[i];
+        if (i%2)    p1[i] = p1[i-1]+arr[i];
+        else        p1[i] = p1[i-2]+(arr[i-1]*arr[i]);
+        if (i>1) {
+            if (i%2)p2[i] = p2[i-2]+(arr[i-1]*arr[i]);
+            else    p2[i] = p2[i-1]+arr[i];
         }
+        res += (arr[i] <= d);
     }
-    else {
-        for (unsigned i = 0; i < n; i++) {
-            cin >> in
-            arr1[n+i] = in;
-            if ((i > 1) && (i < n-1)) arr[n+i-1] = in;
-        }
-    }
-    build(arr1, l); build(arr2, r);
 
-    cout << get(arr1, n>>1, n-1) + get(arr2, (n-1)>>1, n-2);
+    for (unsigned i = 1; i <= n; i++) {
+        auto pos = lower_bound(p1+1, p1+i+1, p1[i]-d) - (p1+1);
+        if (((i-pos) % 2) && pos) pos++;
+        res += (i-pos+1)/2 - ((i%2) && (pos != i));
+    }
+    for (unsigned i = 2; i <= n; i++) {
+        auto pos = lower_bound(p2+2, p2+i+2, p2[i]-d) - (p2+2) + 1;
+        if (((i-pos) % 2) && (pos!=1)) pos++;
+        res += (i-pos+1)/2 - 1 + (i%2);
+    }
+    cout << res;
 }
--- test.cpp
+++ test.cpp
@@ -1,16 +1,77 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-
-int main() {
-    unsigned n, q; cin >> n >> q; 
-    int **arr = new int*[n];
-    for (unsigned i = 0; i < n; i++) {
-        unsigned l; cin >> l; arr[i] = new int[l];
-        for (auto ele = arr[i]; ele <= arr[i] + l; ele++)
-            cin >> *ele;
+typedef long long int ll;
+typedef pair<int,int> ii;
+#define FORW(i,a,b) for(int i=a;i<=b;i++)
+#define FORB(i,a,b) for (int i=a;i>=b;i--)
+#define forw(i,a,b) for (ll i=a;i<=b;i++)
+#define forb(i,a,b) for (ll i=a;i>=b;i--)
+#define fast {ios::sync_with_stdio(false); cin.tie(0); }
+#define fi first
+#define se second
+#define pu push
+#define puf push_front
+#define pb push_back
+#define pof pop_front
+#define pob pop_back
+#define fr front
+#define all(a) a.begin(),a.end()
+#define endl "\n"
+#define db(val) "["#val" = "<<(val)<<"] "
+int dX[] ={0,-1,0,1};
+int dY[]= {-1,0,1,0};
+const ll oo=1e18;
+const ll MOD=1e9+7;
+const ll mod=1e9+9;
+const int mxN=2e5+7;
+ll p1[mxN],p2[mxN],a[mxN];
+int n;
+ll d;
+ll res=0;
+int main()
+{
+    fast;
+    freopen("test.INP","r",stdin);
+    freopen("test.OUT","w",stdout);
+    cin>>n>>d;
+    FORW(i,1,n){
+        int t;
+        cin>>a[i];
+        if (!(i%2))
+         p1[i]=p1[i-2]+(a[i-1]*a[i]);
+        else
+         p1[i]=p1[i-1]+a[i];
     }
-    
-    return 0;
+    FORW(i,2,n){
+        if ((i%2))
+            p2[i]=p2[i-2]+(a[i-1]*a[i]);
+       else
+         p2[i]=p2[i-1]+a[i];
+    }
+
+   map<ll,ll> save1,save2 ;
+    FORW(i,1,n){
+        auto pos = lower_bound(p1+1,p1+i+1,p1[i]-d)-(p1+1);
+        res+=i-pos;
+        if (pos%2) res--;
+        cout<<pos<<endl;
+    }
+    FORW(i,2,n){
+        auto pos = lower_bound(p2+2,p2+i+2,p2[i]-d)-(p2+2);
+        res+=i-pos;
+        if (pos%2) res--;
+        cout<<pos<<endl;
+    }
+    FORW(i,2,n){
+            save2[p1[i]]=i;
+            save1[p2[i]]=i;
+            res+=i-save1[p2[i]-d];
+            res+=i-save2[p1[i]-d];
+    }
+    for (auto v: save1)
+         cout<<v.fi<<": "<<v.se<<endl;
+
+    cout<<(1LL*(ll)round(res/2.0));
+
 }
+
