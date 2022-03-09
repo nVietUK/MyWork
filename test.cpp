9038d5f9cdcd09c8ae2a3e58cc351d8f933dc501
Nguyễn Việt Anh <83152264+nVietUK@users.noreply.github.com>
2022-03-09 12:11:37 +0700

    9/3

--- test.cpp
+++ test.cpp
@@ -1,45 +1,58 @@
-#include <bits/stdc++.h>
-#define ONLINE false
-#define DEBUG true
+#include <iostream>
+#include <sstream>
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<unsigned, unsigned> uu;
-
-template <typename T> struct wrapped_array {
-    wrapped_array(T* first, T* last)
-        : begin_ {first}, end_ {last} {}
-    wrapped_array(T* first, std::ptrdiff_t size)
-        : wrapped_array {first, first + size} {}
-
-    T* begin() const noexcept { return begin_; }
-    T* end() const noexcept { return end_; }
-
-    T* begin_;
-    T* end_;
+class Student {
+    private:
+        int age = -1, standard = -1;
+        string first_name = "", last_name = "";
+    public:
+        void set_age(const int& in) {
+            this->age = in;
+        }
+        void set_standard(const int& in) {
+            this->standard = in;
+        }
+        void set_first_name(const string& in) {
+            this->first_name = in;
+        }
+        void set_last_name(const string& in) {
+            this->last_name = in;
+        }
+        int get_age() {
+            return this->age;
+        }
+        string get_last_name() {
+            return this->last_name;
+        }
+        string get_first_name() {
+            return this->first_name;
+        }
+        int get_standard() {
+            return this->standard;
+        }
+        string to_string() {
+            cout << this->get_age() << "," << this->get_first_name() << "," << this->get_last_name() << "," << this->get_standard();
+        }
 };
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-#if ((__linux__ || _WIN32) && !ONLINE && !ONLINE_JUDGE)
-    freopen("test.inp", "r", stdin);
-    if (!DEBUG) freopen("test.out", "w", stdout);
-#endif
-
-    short a[3], b[3];
-    for (auto&& e : wrapped_array<short>a)
-        cin >> e;
-    for (auto&& e : wrapped_array(a))
-        cin >> e;
-
-    short ca=0, cb=0;
-    for (short i = 0; i < 3; i++) 
-        if (a[i] > b[i])
-            ca++;
-        else 
-        if (a[i] < b[i])
-            cb++;
-    cout << ca << " " << cb;        
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    st.to_string();
+    
+    return 0;
 }
