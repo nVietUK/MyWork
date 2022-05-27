8983fe16047cd2bd3c4a853c0e69ee61cbd93350
nVietUK <vietuk.nguyen.git@icloud.com>
2022-05-27 23:54:56 +0700

    night life

--- bedao_priramid2.cpp
+++ bedao_priramid2.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h> 
 
 using namespace std;
-unsigned Q, N, M, i, res;
+unsigned long long Q, N, M, i, res;
 
 signed main() {
     cin.tie(NULL)->sync_with_stdio(false);
 
     cin >> Q; while(Q--) {
         cin >> N >> M;
-        res = N*M + N + 1;
-        for (i = 2; i < M; i++) 
-            res += N*i*2;
-        cout << res % (unsigned)(1e9+7) << "\n";
+        cout << (1+N*(M*M-1))%(1000000000+7) << "\n";
     }
 }
