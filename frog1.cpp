1cc3cde173da2f770f193e7f93d7bdb7e4cb2a78
nVietUK <vietuk.nguyen.git@icloud.com>
2022-05-30 10:23:14 +0700

    Mon May 30 10:23:14 +07 2022

---
+++ frog1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+signed main() {
+    cin.tie(NULL)->sync_with_stdio(false);
+
+    cin >> N; st = new unsigned*[N+1]; a = new unsigned[N+1];
+    for (i = 1; i <= N && cin >> a[i]; i++);
+}
