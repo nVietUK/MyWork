8796471f9ad49c01edc084a79b8dcb5b714e6f0e
nVietUK <vietuk.nguyen.git@icloud.com>
2022-05-30 20:46:21 +0700

    Mon May 30 20:46:21 +07 2022

--- pwalk.cpp
+++ pwalk.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+struct Edge {
+    int v
+}
+
+
 int main() {
     cin.tie(NULL)->sync_with_stdio(false);
 
     cin >> N >> Q;
+    for (i = 1; i < N; i++) {
+        cin >> a >> b >> l;
+        
+        g[u].
+    }
 }
