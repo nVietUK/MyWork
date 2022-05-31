a6cea965768ad215354b719d308caa00e14b4750
nVietUK <vietuk.nguyen.git@icloud.com>
2022-05-31 08:35:27 +0700

    Tue May 31 08:35:27 +07 2022

--- pwalk.cpp
+++ pwalk.cpp
@@ -5,17 +5,16 @@ using namespace std;
 struct Edge {
     int v, w;
     Edge(int v=0, int w=0) : v(v), w(w) {}
-}
+};
 vector<Edge> *g;
 
 
 int main() {
     cin.tie(NULL)->sync_with_stdio(false);
 
-    cin >> N >> Q;
+    cin >> N >> Q; g = new vector<Edge>[N];
     for (i = 1; i < N; i++) {
         cin >> a >> b >> l;
-        
-        g[u].
+        g[a].push
     }
 }
