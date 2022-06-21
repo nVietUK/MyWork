d4cf4b3bde2cd56e76f701bbbbf3c88bc2a1e180
nVietUK <vietuk.nguyen.git@icloud.com>
2022-06-21 18:32:39 +0700

    Thứ ba, 21 Tháng 6 năm 2022 18:32:39 +07

--- nicknameSuggestion.cpp
+++ nicknameSuggestion.cpp
@@ -1,22 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-struct node {
-    node* next[26];
-} a;
 
-void build(unsigned id, string s, node *a) {
+struct node { node *next['z']; char c=0; } tree;
+string q[1000009], s;
+
+void build(unsigned id, string &s, node *tree) {
     if (id == s.size()) return;
-    if (a->next[s[id]-'a'] == NULL)
-        a->next[s[id]-'a'] = new node;
-    build(id+1, s, a->next[s[id]-'a']);
+    if (tree->next[s[id]] == NULL) {
+        tree->next[s[id]] = new node;
+        tree->next[s[id]]->c = s[id];
+    }
+    build(id+1, s, tree->next[s[id]]);
 }
 
-signed main() { unsigned N;
+unsigned solve(unsigned id, unsigned deep, bool isCheck, string &s, node *tree) {
+    if (tree == NULL || tree->c == '\000') return (isCheck) ? 0 : 1e9; 
+    unsigned a = solve(id, deep+1, true, s, tree->next[s[id]]) + 1,
+             b = solve(id+1, deep+1, false, s, tree->next[s[id+1]]);
+    return min(a, b);
+}
+
+int main() {
     cin.tie(NULL)->sync_with_stdio(false);
 
-    cin >> N; string s, q[1000009];
-    for (unsigned i = 1; i <= N && cin >> s; i++) {
-        build(0, s, &a); q[i] = s;
-    }
+    unsigned N; cin >> N; for (unsigned i = 1; i <= N && cin >> s; i++) 
+        q[i] = s, s+='\000', build(0, s, &tree);
+
+    for (unsigned i = 1; i <= N; i++) 
+        cout << solve(0, 0, true, q[i], tree.next[q[i][0]]) << '\n';
 }
+
