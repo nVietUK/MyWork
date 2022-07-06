0aed4b81c109d041eab1403a925ca1f4bc1b7df4
nVietUK <vietuk.nguyen.git@icloud.com>
2022-07-06 12:04:47 +0700

    Wed Jul  6 12:04:47 PM +07 2022

--- gridpaths.cpp
+++ gridpaths.cpp
@@ -1,10 +1,51 @@
 #include <stdio.h>
+#include <algorithm>
+
+using namespace std;
 
 unsigned p[48];
 bool onp[9][9];
+int dr[4] = {-1, 0, 1, 0},
+    dc[4] = {0, 1, 0, -1};
 
 int tryP(int idx, int cR, int cC) {
-    if ((onp[cR][cC-1] && on
+    if (
+            (
+             (onp[cR][cC-1] && onp[cR][cC+1]) && 
+             (!onp[cR-1][cC] && !onp[cR+1][cC])
+             ) || 
+            (
+             (onp[cR-1][cC] && onp[cR+1][cC]) &&
+             (!onp[cR][cC-1] && !onp[cR][cC+1])
+            )
+       ) {
+        return 0;
+    }
+
+    if (cR == 7 && cC == 1) {
+        if (idx == 48) return 1;
+        return 0;
+    }
+
+    if (idx == 48) return 0;
+
+    int res = 0; onp[cR][cC] = true;
+
+    if (p[idx] < 4) {
+        int nR = cR + dr[p[idx]],
+            nC = cC + dc[p[idx]];
+        if (!onp[nR][nC]) res += tryP(idx+1, nR, nC);
+    }
+    else {
+        for (int i = 0; i < 4; i++) {
+            int nR = cR + dr[i],
+                nC = cC + dc[i];
+            if (onp[nR][nC]) continue;
+            res += tryP(idx+1, nR, nC);
+        }
+    }
+    onp[cR][cC] = false;
+    return res;
 }
 
 signed main() {
@@ -12,7 +53,6 @@ signed main() {
 
     for (unsigned i = 0; i < 48; i++) {
         char c = s[i];
-
         if (c == 'U') p[i]=0;
         else if (c == 'R') p[i]=1;
         else if (c == 'D') p[i]=2;
@@ -24,5 +64,5 @@ signed main() {
     for (unsigned i = 1; i <= 7; i++) for (unsigned j = 1; j <= 7; j++)
         onp[i][j] = false;
 
-    int idx, sR, sC, ans = tryP(idx, sR, sC);
+    printf("%d", tryP(0, 1, 1));
 }
